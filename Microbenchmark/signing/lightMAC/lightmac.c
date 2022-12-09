7f4ebf0b852b00acda0d4ef97f3794a75e0a51e1
hduser <1365519162@qq.com>
2022-12-09 23:32:39 +0800

    加上统计测量信息

--- Microbenchmark/signing/lightMAC/lightmac.c
+++ Microbenchmark/signing/lightMAC/lightmac.c
@@ -28,18 +28,50 @@
 
 
 #include "./present.h"
+
+static int len= 256; //generating size
+module_param(len, int, S_IRUGO);
+
+typedef __u64 block_64;
+typedef __m128i block_128;
+// typedef struct {block_128 rd_key[11]; } AES_KEY;
+static block_128 current_key2;
+static block_64 current_key1[2];
+#define iteration 200000
+#define test_rounds 10
+#define zero_block()          _mm_setzero_si128()
+#define gen_logging_blk(log,ctr) _mm_insert_epi16(_mm_loadu_si128(log), ctr, 0)
+#define xor_block(x,y)        _mm_xor_si128(x,y)
+// blk_cipher 和 key2 都是128位
+// AES 一共 10轮，和QuickLog保持一致
+// todo 这里10轮采用的key都是同一个，后期根据需要修改
+// #define ek2(blk_cipher, key2)  _mm_aesenc_si128(blk_cipher, key2)
+#define ek2(blk_cipher, key)                            \
+    do{                                                 \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher = _mm_aesenc_si128(blk_cipher, key); \
+		blk_cipher =_mm_aesenclast_si128(blk_cipher, key);\
+	}while(0)
+
 /**  
-* MAC, signing a log message and updating the signing-key & state
+* light, signing a log message and updating the signing-key & state
 * Input @log_msg: a log data,  
         @msg_len: the length of the log data 
 * Computing block format: a block(16 bytes) contains "<i>||M_i",  
 *                         2 bytes counter(<i>) and 14 bytes log data(M_i)
-* Output: T(64-byte tag)
+* Output: T(128-byte tag)
 
 传进来的分成 64 = 8*8一个块
-1个字节表示位置编码
+EK1 和Ek2 分别为PRESENT和AES
 **/
-// PRESENT和AES实例化了LightMAC
+
 /**
  * void present_rounds(const uint8_t *plain, const uint8_t *key, const uint8_t rounds, uint8_t *uint8_t *cipher)
  * 80-bit secret
@@ -47,18 +79,6 @@
  * PRESENT使用64位标记，AES使用128位
  * 分块的时候，按照128分块，每个块分两次送入present进行加密
 */
-#define gen_logging_blk(log,ctr) _mm_insert_epi16(_mm_loadu_si128(log), ctr, 0)
-#define xor_block(x,y)        _mm_xor_si128(x,y)
-#define ek2(blk_cipher, key2)  _mm_aesenc_si128(blk_cipher, key2)
-
-typedef __u64 block_64;
-typedef __m128i block_128;
-static block_128 current_key2;
-static block_64 current_key1[2];
-#define zero_block()          _mm_setzero_si128()
-// static block_128 Ek1(Block_128 block_128){
-
-// }
 static block_128 mac_core(const char *log_msg, const int msg_len){
 	uint16_t remaining = msg_len;
 
@@ -87,28 +107,30 @@ static block_128 mac_core(const char *log_msg, const int msg_len){
     
     unsigned char my_pad[16];
     if(remaining){
-        pr_info("remaining=%d", remaining);
+        // pr_info("remaining=%d", remaining);
         // 末尾拼接上10*
         memcpy(my_pad, log_msg, remaining); // 0,1 放的是位置编码
         my_pad[remaining] = '1';
         for(int i = remaining+1;i < 16;i++){
             my_pad[i] = '0';
         }
-        pr_info("after padding....");
-        for(int i = 0;i < 16;i++){
-            pr_info("i= %d, val = %c ", i, my_pad[i]);
-        }
+        // pr_info("after padding....");
+        // for(int i = 0;i < 16;i++){
+        //     pr_info("i= %d, val = %c ", i, my_pad[i]);
+        // }
         temp_tag_128 = xor_block(temp_tag_128, *(block_128*)my_pad);
     }else{
-        pr_info("No remaining");
+        // pr_info("No remaining");
     }
     
-    temp_tag_128 = _mm_aesenc_si128(temp_tag_128, current_key2);  // 最后结果经过aes，可以截取前t位
+    ek2(temp_tag_128, current_key2);  // 最后结果经过aes，可以截取前t位
     
-    // 更新密钥
+    // @todo 更新密钥
     
     return temp_tag_128;
 }
+
+
 void print_str(char* str,int len)
 {
     pr_info("msg:");
@@ -141,30 +163,106 @@ void print_block128(block_128 var)
 	}
 	pr_info("\n");
 }
-/*Initial
-初始化初始密钥
+/*Initial  初始化初始密钥
 */
 static void crypto_int(void)
 {
 	
 }
+static int compare(const void* a, const void* b)
+{
+    unsigned long long arg1 = *(unsigned long long *)a;
+    unsigned long long  arg2 = *(unsigned long long *)b;
+ 
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+/**
+ * 第二个参数是 long long的数组
+ * 第一个参数是它的长度
+*/
+unsigned long long median(size_t n, unsigned long long * x) {
+    //unsigned long long temp;
+    sort(x, n, sizeof(unsigned long long), &compare, NULL);
+
+    if(n%2==0) {
+        // if there is an even number of elements, return mean of the two elements in the middle
+        return ((x[n/2] + x[n/2 - 1]) / 2);
+    } else {
+        // else return the element in the middle
+        return  x[n/2];
+    }
+}
+// round_time[i] 是每i条日志消息的耗费时间，round_median[i] 是每i轮的【平均】单条日志加密时间
+static unsigned long long single_msg_time[1048576], round_median[test_rounds];
 
 static int __init benchmarking(void){    
-    char *msg1,*msg2;
-    int len1 = 28,len2 = 24; // 一个块14 个字节,msg1刚好分成两个块, msg2最后一个块需要填充10*
-    msg1 = kmalloc(len1, GFP_KERNEL);
-    msg2 = kmalloc(len2, GFP_KERNEL);
-	memset(msg1,'a',len1); 
-    memset(msg2,'b',len2); 
-	print_str(msg1, len1);
-    print_str(msg2, len2);
+    // char *msg1,*msg2;
+    // int len1 = 28,len2 = 24; // 一个块14 个字节,msg1刚好分成两个块, msg2最后一个块需要填充10*
+    // msg1 = kmalloc(len1, GFP_KERNEL);
+    // msg2 = kmalloc(len2, GFP_KERNEL);
+	// memset(msg1,'a',len1); 
+    // memset(msg2,'b',len2); 
+	// print_str(msg1, len1);
+    // print_str(msg2, len2);
 
-    block_128 ret1 = mac_core(msg1, len1);
-    block_128 ret2 = mac_core(msg2, len2);
-    pr_info("ret1:");
-    print_block128(ret1);
-    pr_info("ret2:");
-    print_block128(ret2);
+    // block_128 ret1 = mac_core(msg1, len1);
+    // block_128 ret2 = mac_core(msg2, len2);
+    // pr_info("ret1:");
+    // print_block128(ret1);
+    // pr_info("ret2:");
+    // print_block128(ret2);
+
+
+
+
+    crypto_int();
+
+    char *str; 
+    int len;
+    str = kmalloc(len, GFP_KERNEL);
+    memset(str,'a',(len));
+    msleep(100);
+    pr_info("\n Start .....\n");
+    /*************************************LightMac*************************************************/	
+    unsigned long long  start_time, end_time;
+	block_128  light_tag;
+    int i, j;
+    for(i=0;i<test_rounds;i++){
+		for(j=0;j<iteration;j++)
+		{	
+            /**
+             * 这里的kernel_fpu_begin && kernel_fpu_end 是必须的
+             * 因为 Linux 内核为了提高系统的运行速率，在任务上下文切换时，只会保存/恢复普通寄存器的值，并不包括 FPU 浮点寄存器的值，
+             * 而调用 kernel_fpu_begin 主要作用是【关掉系统抢占】，
+             * 浮点计算结束后调用 kernel_fpu_end 开启系统抢占，
+             * 这使得代码不会被中断，从而安全的进行浮点运算，并且要求这之间的代码不能有休眠或调度操作，
+             * 另外不得有嵌套的情况出现（将会覆盖原始保存的状态，然后执行 kernel_fpu_end() 最终将恢复错误的 FPU 状态）。
+            */
+            start_time = ktime_get_ns();
+			kernel_fpu_begin();
+			light_tag = mac_core(str, len);
+			kernel_fpu_end();
+			end_time = ktime_get_ns();
+			single_msg_time[j] = end_time - start_time;
+		}
+		round_median[i] =  median(iteration,  single_msg_time);  
+		msleep(100);
+	}
+
+    unsigned long long  mean = 0, q_sd = 0, sd_sum = 0, sum = 0;
+    for(int i = 0;i < test_rounds;i++){
+        sum += round_median[i];
+    }
+    mean = sum / test_rounds; // 平均值
+
+    for(int i = 0;i < test_rounds;i++){
+        sd_sum += (round_median[i] - mean) * (round_median[i] - mean);
+
+    }
+    q_sd = int_sqrt(sd_sum / test_rounds); // 均方差
+
     return 0;
 }
 
